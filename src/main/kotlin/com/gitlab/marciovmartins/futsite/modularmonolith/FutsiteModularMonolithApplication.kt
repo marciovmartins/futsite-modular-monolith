6e5d78a8b7d09c9b935045792455a405316eca29
Marcio Vinicius <marciovmartins@hotmail.com>
2022-09-05 22:02:23 +0200

    Initialised ADR and basic maven structure with spring boot application version 2.7.3 setup

---
+++ src/main/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/FutsiteModularMonolithApplication.kt
@@ -0,0 +1,11 @@
+package com.gitlab.marciovmartins.futsite.modularmonolith
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FutsiteModularMonolithApplication
+
+fun main(args: Array<String>) {
+	runApplication<FutsiteModularMonolithApplication>(*args)
+}
---
+++ src/test/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/FutsiteModularMonolithApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gitlab.marciovmartins.futsite.modularmonolith
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FutsiteModularMonolithApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
