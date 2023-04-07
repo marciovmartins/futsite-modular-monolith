83ddb42a7cecd2139ca746492ab1c3ad8a123be9
Marcio Vinicius <marciovmartins@hotmail.com>
2023-04-07 20:53:12 +0200

    wip implemented CalculateRankingTest.with many gameday with many matches registered

--- src/test/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/application/CalculateRankingTest.kt
+++ src/test/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/application/CalculateRankingTest.kt
@@ -137,6 +137,176 @@ internal class CalculateRankingTest {
         val playerId1 = PlayerId(UUID.randomUUID())
         val playerId2 = PlayerId(UUID.randomUUID())
 
+        val gameday1 = Gameday(
+            gamedayId = UUID.randomUUID(),
+            amateurSoccerGroupId = amateurSoccerGroupId,
+            date = Instant.now().minus(2, ChronoUnit.DAYS),
+            matches = listOf(
+                Gameday.Match(
+                    matchId = 1,
+                    players = setOf(
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 2,
+                            playerId = playerId1,
+                            team = Gameday.Match.Team.A,
+                            goalsInFavor = 1u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 3,
+                            playerId = playerId2,
+                            team = Gameday.Match.Team.B,
+                            goalsInFavor = 0u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                    ),
+                ),
+                Gameday.Match(
+                    matchId = 4,
+                    players = setOf(
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 5,
+                            playerId = playerId1,
+                            team = Gameday.Match.Team.A,
+                            goalsInFavor = 2u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 6,
+                            playerId = playerId2,
+                            team = Gameday.Match.Team.B,
+                            goalsInFavor = 0u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        val gameday2 = Gameday(
+            gamedayId = UUID.randomUUID(),
+            amateurSoccerGroupId = amateurSoccerGroupId,
+            date = Instant.now().minus(1, ChronoUnit.DAYS),
+            matches = listOf(
+                Gameday.Match(
+                    matchId = 7,
+                    players = setOf(
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 8,
+                            playerId = playerId1,
+                            team = Gameday.Match.Team.A,
+                            goalsInFavor = 0u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 9,
+                            playerId = playerId2,
+                            team = Gameday.Match.Team.B,
+                            goalsInFavor = 0u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                    ),
+                ),
+                Gameday.Match(
+                    matchId = 10,
+                    players = setOf(
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 11,
+                            playerId = playerId1,
+                            team = Gameday.Match.Team.A,
+                            goalsInFavor = 0u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                        Gameday.Match.PlayerStatistic(
+                            playerStatisticId = 12,
+                            playerId = playerId2,
+                            team = Gameday.Match.Team.B,
+                            goalsInFavor = 1u,
+                            goalsAgainst = 0u,
+                            yellowCards = 0u,
+                            blueCards = 0u,
+                            redCards = 0u,
+                        ),
+                    ),
+                ),
+            ),
+        )
+
+        val gamedayRepository = FakeGamedayRepository(
+            mutableMapOf(
+                gameday1.gamedayId!! to gameday1,
+                gameday2.gamedayId!! to gameday2,
+            )
+        )
+
+        val expectedRanking = RankingDTO(
+            amateurSoccerGroupId = amateurSoccerGroupId,
+            period = period,
+            matches = 4u,
+            playerStatistics = setOf(
+                RankingDTO.PlayerStatistic(
+                    playerId = playerId1.value,
+                    matches = 4u,
+                    victories = 2u,
+                    draws = 1u,
+                    defeats = 1u,
+                    goalsInFavor = 3u,
+                    ownGoals = 0u,
+                ),
+                RankingDTO.PlayerStatistic(
+                    playerId = playerId2.value,
+                    matches = 4u,
+                    victories = 1u,
+                    draws = 1u,
+                    defeats = 2u,
+                    goalsInFavor = 1u,
+                    ownGoals = 0u,
+                ),
+            ),
+        )
+
+        val calculateRanking = CalculateRanking(gamedayRepository)
+
+        // when
+        val actualRanking = calculateRanking.with(amateurSoccerGroupId, period)
+
+        // then
+        assertThat(actualRanking).isEqualTo(expectedRanking)
+    }
+
+    @Test
+    fun `with many gameday with many matches registered`() {
+        // given
+        val amateurSoccerGroupId = UUID.randomUUID()
+        val period = RankingDTO.Period(
+            from = Instant.now().minus(7, ChronoUnit.DAYS),
+            to = Instant.now()
+        )
+
+        val playerId1 = PlayerId(UUID.randomUUID())
+        val playerId2 = PlayerId(UUID.randomUUID())
+
         val gameday = Gameday(
             gamedayId = UUID.randomUUID(),
             amateurSoccerGroupId = amateurSoccerGroupId,
@@ -234,10 +404,5 @@ internal class CalculateRankingTest {
 
         // then
         assertThat(actualRanking).isEqualTo(expectedRanking)
-    }
-
-    @Test
-    fun `with many gameday with many matches registered`() {
-        TODO("need to be implemented")
     }
 }
