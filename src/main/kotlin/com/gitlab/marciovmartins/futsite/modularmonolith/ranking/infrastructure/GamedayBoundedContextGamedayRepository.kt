a6f103978a0271d7cd760ad43c6e3af3758e10f3
Marcio Vinicius <marciovmartins@hotmail.com>
2023-04-16 19:42:33 +0200

    create GamedayRepositoryIT to find one gameday by amateur soccer group id and period

--- src/main/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/infrastructure/GamedayBoundedContextGamedayRepository.kt
+++ src/main/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/infrastructure/GamedayBoundedContextGamedayRepository.kt
@@ -3,12 +3,40 @@ package com.gitlab.marciovmartins.futsite.modularmonolith.ranking.infrastructure
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.AmateurSoccerGroupId
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.Gameday
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.GamedayRepository
+import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.PlayerId
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.Ranking
 
 class GamedayBoundedContextGamedayRepository(
     private val gamedayRepository: com.gitlab.marciovmartins.futsite.modularmonolith.gameday.GamedayRepository
 ) : GamedayRepository {
     override fun findBy(amateurSoccerGroupId: AmateurSoccerGroupId, period: Ranking.Period): Set<Gameday> {
-        return emptySet()
+        return gamedayRepository.findByAmateurSoccerGroupIdAndDateAfterAndDateBefore(
+            amateurSoccerGroupId.value, period.from, period.to,
+        )
+            .map { mapTo(it) }
+            .toSet()
+    }
+
+    private fun mapTo(gamedayEntity: com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday): Gameday {
+        return Gameday(
+            amateurSoccerGroupId = AmateurSoccerGroupId(gamedayEntity.amateurSoccerGroupId!!),
+            date = Gameday.Date(gamedayEntity.date),
+            matches = gamedayEntity.matches.map { mapTo(it) },
+        )
+    }
+
+    private fun mapTo(matchEntity: com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match): Gameday.Match {
+        return Gameday.Match(
+            players = matchEntity.players.map { mapTo(it) }.toSet(),
+        )
+    }
+
+    private fun mapTo(playerStatistic: com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match.PlayerStatistic): Gameday.Match.PlayerStatistic {
+        return Gameday.Match.PlayerStatistic(
+            playerId = PlayerId(playerStatistic.playerId),
+            team = Gameday.Match.Team.valueOf(playerStatistic.team.name),
+            goalsInFavor = playerStatistic.goalsInFavor,
+            ownGoals = playerStatistic.ownGoals,
+        )
     }
 }
--- src/test/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/infrastructure/GamedayRepositoryIT.kt
+++ src/test/kotlin/com/gitlab/marciovmartins/futsite/modularmonolith/ranking/infrastructure/GamedayRepositoryIT.kt
@@ -1,6 +1,9 @@
 package com.gitlab.marciovmartins.futsite.modularmonolith.ranking.infrastructure
 
+import com.gitlab.marciovmartins.futsite.modularmonolith.amateursoccergroup.AmateurSoccerGroup
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.AmateurSoccerGroupId
+import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.Gameday
+import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.PlayerId
 import com.gitlab.marciovmartins.futsite.modularmonolith.ranking.domain.Ranking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -10,10 +13,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.UUID
+import kotlin.random.Random
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class GamedayRepositoryIT(
+    @Autowired private val jpaAmateurSoccerGroup: com.gitlab.marciovmartins.futsite.modularmonolith.amateursoccergroup.AmateurSoccerGroupRepository,
     @Autowired private val jpaGamedayRepository: com.gitlab.marciovmartins.futsite.modularmonolith.gameday.GamedayRepository
 ) {
     @Test
@@ -32,5 +37,94 @@ class GamedayRepositoryIT(
 
         // then
         assertThat(gamedays).isEmpty()
+    }
+
+    @Test
+    fun `find one gameday by amateur soccer group id and period`() {
+        // given
+        val amateurSoccerGroupId = AmateurSoccerGroupId(UUID.randomUUID())
+        val period = Ranking.Period(
+            from = Instant.now().minus(7, ChronoUnit.DAYS),
+            to = Instant.now(),
+        )
+
+        val amateurSoccerGroup = AmateurSoccerGroup(
+            amateurSoccerGroupId = amateurSoccerGroupId.value,
+            name = "amateur-soccer-group-id ${Random.nextLong()}"
+        )
+        jpaAmateurSoccerGroup.save(amateurSoccerGroup)
+
+        val date = Instant.now().minus(1, ChronoUnit.DAYS)
+        val playerId1 = UUID.randomUUID()
+        val playerId2 = UUID.randomUUID()
+        val gameday = com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday(
+            gamedayId = UUID.randomUUID(),
+            amateurSoccerGroupId = amateurSoccerGroupId.value,
+            date = date,
+            matches = listOf(
+                com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match(
+                    matchId = Random.nextLong(1, 99999999),
+                    players = setOf(
+                        com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match.PlayerStatistic(
+                            playerStatisticId = Random.nextLong(1, 99999999),
+                            playerId = playerId1,
+                            team = com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match.Team.A,
+                            goalsInFavor = 5u,
+                            ownGoals = 4u,
+                            yellowCards = 3u,
+                            blueCards = 2u,
+                            redCards = 1u,
+                        ),
+                        com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match.PlayerStatistic(
+                            playerStatisticId = Random.nextLong(1, 99999999),
+                            playerId = playerId2,
+                            team = com.gitlab.marciovmartins.futsite.modularmonolith.gameday.Gameday.Match.Team.B,
+                            goalsInFavor = 4u,
+                            ownGoals = 3u,
+                            yellowCards = 2u,
+                            blueCards = 1u,
+                            redCards = 0u,
+                        ),
+                    ),
+                )
+            )
+        )
+        jpaGamedayRepository.save(gameday)
+
+        val gamedayRepository = GamedayBoundedContextGamedayRepository(jpaGamedayRepository)
+
+        val expectedGamedays = setOf(
+            Gameday(
+                amateurSoccerGroupId = amateurSoccerGroupId,
+                date = Gameday.Date(date),
+                matches = listOf(
+                    Gameday.Match(
+                        players = setOf(
+                            Gameday.Match.PlayerStatistic(
+                                playerId = PlayerId(playerId1),
+                                team = Gameday.Match.Team.A,
+                                goalsInFavor = 5u,
+                                ownGoals = 4u,
+                            ),
+                            Gameday.Match.PlayerStatistic(
+                                playerId = PlayerId(playerId2),
+                                team = Gameday.Match.Team.B,
+                                goalsInFavor = 4u,
+                                ownGoals = 3u,
+                            ),
+                        )
+                    )
+                ),
+            )
+        )
+
+        // when
+        val gamedays = gamedayRepository.findBy(amateurSoccerGroupId, period)
+
+        // then
+        assertThat(gamedays).hasSize(1)
+            .usingRecursiveComparison()
+            .ignoringCollectionOrder()
+            .isEqualTo(expectedGamedays)
     }
 }
